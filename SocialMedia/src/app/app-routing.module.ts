a36734426f8316e1cc1eed52fd473cd91c6b39d7
KayAreBe <kjkoenaite@gmail.com>
2023-05-10 09:27:12 +0200

    updates

--- SocialMedia/src/app/app-routing.module.ts
+++ SocialMedia/src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './component/login/login.component';
 import { AddPostComponent } from './component/add-post/add-post.component';
 import { RegisterComponent } from './component/register/register.component';
+import { HomeComponent } from './component/home/home.component';
+import { ForgotPasswordComponent } from './component/forgot-password/forgot-password.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'add', component: AddPostComponent },
-  { path: 'register', component: RegisterComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'add', component: AddPostComponent }
 ];
 
 @NgModule({
