fff901f069b33a636d2f3cbeeee64a80fbf0e040
KayAreBe <kjkoenaite@gmail.com>
2023-05-11 08:38:05 +0200

    updates commit

--- SocialMedia/src/app/app-routing.module.ts
+++ SocialMedia/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './component/home/home.component';
 import { ForgotPasswordComponent } from './component/forgot-password/forgot-password.component';
 
 const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch:'full'},
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
   { path: 'register', component: RegisterComponent },
